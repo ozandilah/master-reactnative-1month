f1bde41dd0007db38fd4971d99cee5ab3dccfea1
ozandilah <ozan.it212@gmail.com>
2024-06-15 00:27:29 +0700

    menambahkan tag flatlist untuk scrolling data dengan map

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  console.log("Hello Brother");
+  const days = [1, 2, 3, 4, 5];
   return (
     <View style={styles.container}>
-      <Text>
-        Hello brother, this is my first make android with react native
-      </Text>
+      <FlatList
+        data={days}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +25,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#F9EDE3",
+    width: 300,
+    height: 300,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9B4521",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#9B4521",
+    fontSize: 50,
   },
 });
