e40e2d7cd67e4a971e2cb50815f736b75e9bb7b9
ozandilah <ozan.it212@gmail.com>
2024-07-08 15:42:15 +0700

    Setup MapView and Marker using react-native-maps packages

--- src/components/core/DayListItem.tsx
+++ src/components/core/DayListItem.tsx
@@ -16,17 +16,17 @@ export default function DayListItem({ day }: Props) {
 }
 const styles = StyleSheet.create({
   box: {
-    backgroundColor: "#F9EDE3",
+    backgroundColor: "#111",
     flex: 1,
     aspectRatio: 1,
     borderWidth: StyleSheet.hairlineWidth,
-    borderColor: "#9B4521",
+    borderColor: "#fff",
     borderRadius: 20,
     justifyContent: "center",
     alignItems: "center",
   },
   text: {
-    color: "#9B4521",
+    color: "#fff",
     fontSize: 75,
     fontFamily: "AmaticBold",
   },
