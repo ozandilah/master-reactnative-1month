49a5562ebafd3a3a2a99b73b2e452d7c4b5dca61
ozandilah <ozan.it212@gmail.com>
2024-07-15 23:42:43 +0700

    move gesture

--- src/app/(days)/day7/tinder.tsx
+++ src/app/(days)/day7/tinder.tsx
@@ -1,9 +1,12 @@
 import TinderCard from "@/components/day7/TinderCard";
 import { Stack } from "expo-router";
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import { useSharedValue } from "react-native-reanimated";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useAnimatedReaction,
+  useSharedValue,
+  runOnJS,
+} from "react-native-reanimated";
 
 const dummuUsers = [
   {
@@ -45,33 +48,34 @@ const dummuUsers = [
 ];
 const TinderScreen = () => {
   const activeIndex = useSharedValue(0);
-  const translationX = useSharedValue(0);
+  const [index, setIndex] = useState(0);
 
-  const gesture = Gesture.Pan()
-    .onBegin((event) => console.log("onBegin:"))
-    .onStart((event) => console.log("onStart:"))
-    .onChange((event) => {
-      translationX.value = event.translationX;
-    })
-    .onEnd((event) => console.log("onEnd:"))
-    .onFinalize((event) => console.log("onFinalize:"));
+  useAnimatedReaction(
+    () => activeIndex.value,
+    (value, preValue) => {
+      if (Math.floor(value) !== index) {
+        runOnJS(setIndex)(Math.floor(value));
+      }
+    }
+  );
+
   return (
-    <GestureDetector gesture={gesture}>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Stack.Screen options={{ headerShown: false }} />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Text style={{ top: 70, position: "absolute", textAlign: "center" }}>
+        Current Index: {index}
+      </Text>
 
-        {dummuUsers.map((user, index) => (
-          <TinderCard
-            user={user}
-            key={user.id}
-            numOfCards={dummuUsers.length}
-            index={index}
-            activeIndex={activeIndex}
-            translationX={translationX}
-          />
-        ))}
-      </View>
-    </GestureDetector>
+      {dummuUsers.map((user, index) => (
+        <TinderCard
+          user={user}
+          key={user.id}
+          numOfCards={dummuUsers.length}
+          index={index}
+          activeIndex={activeIndex}
+        />
+      ))}
+    </View>
   );
 };
 
--- src/components/day7/TinderCard.tsx
+++ src/components/day7/TinderCard.tsx
@@ -5,7 +5,11 @@ import Animated, {
   SharedValue,
   interpolate,
   useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 interface TinderInterface {
   user: {
@@ -15,16 +19,17 @@ interface TinderInterface {
   numOfCards: number;
   index: number;
   activeIndex: SharedValue<number>;
-  translationX: SharedValue<number>;
 }
-
-export const tinderCardWith = Dimensions.get("screen").width * 0.8;
+const screenWidth = Dimensions.get("screen").width;
+export const tinderCardWith = screenWidth * 0.8;
 const TinderCard = ({
   user,
   numOfCards,
   index,
   activeIndex,
 }: TinderInterface) => {
+  const translationX = useSharedValue(0);
+
   const animatedCard = useAnimatedStyle(() => ({
     opacity: interpolate(
       activeIndex.value,
@@ -46,32 +51,66 @@ const TinderCard = ({
           [-30, 0, 0]
         ),
       },
+      {
+        translateX: translationX.value,
+      },
+      {
+        rotateZ: `${interpolate(
+          translationX.value,
+          // [-tinderCardWith / 2, 0, tinderCardWith / 2],
+          [-screenWidth / 2, 0, screenWidth / 2],
+          // [-30, 0, 30]
+          [-15, 0, 15]
+        )}deg`,
+      },
     ],
   }));
+  const gesture = Gesture.Pan()
+    .onChange((event) => {
+      translationX.value = event.translationX;
+      activeIndex.value = interpolate(
+        Math.abs(translationX.value),
+        [0, 500],
+        [index, index + 0.8]
+      );
+    })
+    .onEnd((event) => {
+      if (Math.abs(event.velocityX) > 400) {
+        translationX.value = withSpring(Math.sign(event.velocityX) * 500, {
+          velocity: event.velocityX,
+        });
+        activeIndex.value = withSpring(index + 1);
+      } else {
+        translationX.value = withSpring(0);
+      }
+    })
+    .onFinalize((event) => console.log("onFinalize:"));
   return (
-    <Animated.View
-      style={[
-        styles.card,
-        animatedCard,
-        {
-          zIndex: numOfCards - index,
-          opacity: 1 - index * 0.2,
-        },
-      ]}
-    >
-      <Image
-        style={[StyleSheet.absoluteFillObject, styles.image]}
-        source={{ uri: user.image }}
-      />
-      <LinearGradient
-        // Background Linear Gradient
-        colors={["transparent", "rgba(0,0,0,0.8)"]}
-        style={[StyleSheet.absoluteFillObject, styles.overflay]}
-      />
-      <View style={styles.footer}>
-        <Text style={styles.name}> {user.name}</Text>
-      </View>
-    </Animated.View>
+    <GestureDetector gesture={gesture}>
+      <Animated.View
+        style={[
+          styles.card,
+          animatedCard,
+          {
+            zIndex: numOfCards - index,
+            opacity: 1 - index * 0.2,
+          },
+        ]}
+      >
+        <Image
+          style={[StyleSheet.absoluteFillObject, styles.image]}
+          source={{ uri: user.image }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", "rgba(0,0,0,0.8)"]}
+          style={[StyleSheet.absoluteFillObject, styles.overflay]}
+        />
+        <View style={styles.footer}>
+          <Text style={styles.name}> {user.name}</Text>
+        </View>
+      </Animated.View>
+    </GestureDetector>
   );
 };
 
