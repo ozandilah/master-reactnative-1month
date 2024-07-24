be898a51bc1bd14cead0dea0bc84eb31e699dc10
ozandilah <ozan.it212@gmail.com>
2024-07-24 22:29:25 +0700

    agenda

--- src/app/(days)/day8/index.tsx
+++ src/app/(days)/day8/index.tsx
@@ -12,6 +12,7 @@ Authentication in React Native using AWS Amplify V6
 - Set up Amplify project
 - Add Authentication to app
 - Build your custom Auth Screens
+- Portfolio amplify aws feature
 `;
 
 const DayDetailScreens = () => {
